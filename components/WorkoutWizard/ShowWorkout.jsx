ed02eeba8c19be2f4b3f40f2e564ce8b822d01fe
Xavier Del Castillo <xavier.delcastillo@commit.dev>
2021-07-22 08:41:27 -0700

    UI adjustments

--- components/WorkoutWizard/ShowWorkout.jsx
+++ components/WorkoutWizard/ShowWorkout.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 
 import Button from '@material-ui/core/Button';
 import Paper from '@material-ui/core/Paper';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 import Typography from '@material-ui/core/Typography';
 
+const useStyles = makeStyles(() => ({
+  dayList: {
+    margin: "30px 0"
+  }
+}))
+
 export default function ShowWorkout(props) {
+  const styles = useStyles();
   const workout = props.workout;
 
   return (
@@ -12,15 +23,18 @@ export default function ShowWorkout(props) {
       <Typography align="center" variant="h5">This is your current workout.</Typography>
       <Typography align="center" variant="subtitle1">{`Workout name: ${workout.schedule.name}`}.</Typography>
       {Object.entries(workout.days).map(([key, day]) => {
-        console.log(key, day);
         return (
-          <Paper key={key} elevation={3}>
-            <div>{day.name}</div>
-            <ul>
+          <Paper key={key} elevation={3} className={styles.dayList}>
+            <Typography align="center" variant="subtitle2">{day.name}</Typography>
+            <List dense={true}>
               {day.exercises.map((exercise, i) => {
-                return (<li key={key + i}>{exercise.name}</li>);
+                return (
+                  <ListItem key={key + i}>
+                    <ListItemText primary={exercise.name} item/>
+                  </ListItem>
+                );
               })}
-            </ul>
+            </List>
           </Paper>
         )
       })}
